eea698fbaf77f3678f613ede9dffd6aecfa39cd8
ahmdsaza <ahmdsaza@gmail.com>
2024-09-05 05:24:12 +0300

    Some changes

--- database/migrations/2024_08_14_101854_create_rates_table.php
+++ database/migrations/2024_08_14_101854_create_rates_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('product_id');
             $table->integer('product_rate');
             $table->string('status');
-            $table->string('description')->default(' ');
+            $table->string('description')->nullable()->default(' ');
             $table->timestamps();
         });
     }
